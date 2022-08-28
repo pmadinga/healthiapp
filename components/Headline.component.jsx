4c36f6fdcea08a45168ddb267e6208d151c8f593
Phiwa Madinga <phiwa@koedr.org>
2022-08-28 22:50:44 +0200

    Styled banner

--- components/Headline.component.jsx
+++ components/Headline.component.jsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
 import React from 'react';
-import banner from '../assets/imgs/Illustration.png'
+import styles from '../assets/styles/Headline.component.module.css'
+import bannerImg from '../assets/imgs/Illustration.png'
+
+
 const HeadlineComponent = () => {
   return (
     <div>
-      <div className="">
-        <h1>Eat . Live . Healthi</h1>
-        <Image src={banner} width={100} height={100} alt='Banner Image'/>
+      <div className={styles.banner}>
+        <h1 className={styles.banner_headline}>Eat . Live . Healthi</h1>
+        <Image src={bannerImg} width={300} height={300} alt='Banner Image'/>
       </div>
     </div>
   );
