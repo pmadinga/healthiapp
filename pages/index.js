fc25d2bb84cfb9b3dd5342b064bd6e17a486306d
Phiwa Madinga <phiwa@koedr.org>
2022-08-28 23:28:01 +0200

    create search component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import HeadlineComponent from '../components/Headline.component'
+import SearchComponent from '../components/Search.component'
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <title>HealthiApp | Home</title>
       </Head>
       <HeadlineComponent/>
+      <SearchComponent/>
     </>
   )
 }
